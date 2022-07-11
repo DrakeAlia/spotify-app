811383509d935cd8da3816173fb4ca65445c82aa
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-07-11 12:37:22 -0700

    added sidebar file in components folder

---
+++ components/sidebar.tsx
@@ -0,0 +1,8 @@
+import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay } from "@chakra-ui/layout";
+import { MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite } from "react-icons/md";
+
+const Sidebar = () => {
+    return <div>hello from sidebar</div>
+}
+
+export default Sidebar;
