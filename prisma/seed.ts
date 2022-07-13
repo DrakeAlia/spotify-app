26db36b75073771c48b2ca987e876984742e7806
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-07-12 22:36:16 -0700

    seeding data

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client'
+import bcrpyt from 'bcrypt'
+import { artistsData } from './songsData'
+
+const prisma = new PrismaClient()
+
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            })),
+          },
+        },
+      })
+    })
+  )
+}
+
+run()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
