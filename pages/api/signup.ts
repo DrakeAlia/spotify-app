eab9a7ec32e1b3c7f7b8145f93d7d7d19ecf0ff4
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-07-13 14:15:04 -0700

    serverless api overview

---
+++ pages/api/signup.ts
@@ -0,0 +1,9 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import cookie from 'cookie'
+import prisma from '../../lib/prisma'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    
+}
