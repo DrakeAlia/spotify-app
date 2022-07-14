6d648113b642e5f7ccb469e7e148914ede872307
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-07-14 12:05:43 -0700

    custom hooksfor fetching data

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -11,11 +11,11 @@ export const useMe = () => {
   }
 }
 
-export const usePlayList = () => {
+export const usePlaylist = () => {
   const { data, error } = useSWR('/playlist', fetcher)
 
   return {
-    playlist: data || [],
+    playlists: (data as any) || [],
     isLoading: !data && !error,
     isError: error,
   }
