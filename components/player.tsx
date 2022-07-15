a3e8acb5a7957504eaccf98ccbd8a26d79f2e35b
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-07-15 16:17:43 -0700

    player controls ui

---
+++ components/player.tsx
@@ -0,0 +1,81 @@
+import {
+  ButtonGroup,
+  Box,
+  IconButton,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderTrack,
+  RangeSliderThumb,
+  Center,
+  Flex,
+  Text,
+} from '@chakra-ui/react'
+import ReactHowler from 'react-howler'
+import { useEffect, useRef, useState } from 'react'
+import {
+  MdShuffle,
+  MdSkipPrevious,
+  MdSkipNext,
+  MdOutlinePlayCircleFilled,
+  MdOutlinePauseCircleFilled,
+  MdOutlineRepeat,
+} from 'react-icons/md'
+import { useStoreActions } from 'easy-peasy'
+
+const Player = () => {
+  return (
+    <Box>
+      <Box>{/* <ReactHowler /> */}</Box>
+      <Center color="gray.600">
+        <ButtonGroup>
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdShuffle />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="skip"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="play"
+            fontSize="40px"
+            color="white"
+            icon={<MdOutlinePlayCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="pause"
+            fontSize="40px"
+            color="white"
+            icon={<MdOutlinePauseCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="next"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="repeat"
+            fontSize="24px"
+            icon={<MdOutlineRepeat />}
+          />
+        </ButtonGroup>
+      </Center>
+    </Box>
+  )
+}
+
+export default Player
