14032dafe413814674daffdc8b9300292be7aab2
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-07-14 22:26:14 -0700

    seed user data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import { Box, Text, Flex } from '@chakra-ui/layout'
 import { Image } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout'
+import { useMe } from '../lib/hooks'
 import prisma from '../lib/prisma'
 
 const Home = ({ artists }) => {
+  const { user } = useMe()
+
   return (
     <GradientLayout
       roundImage
-      color="red"
+      color="gray"
       subtitle="profile"
-      title="Drake Alia"
-      description="15 public playlists"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistsCount} public playlists`}
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
     >
       <Box color="white" paddingX="40px">
