8ca877b3219f45e88d34b002b41f6edb9e1f789e
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-07-16 08:17:59 -0700

    shuffle and next song controls

--- components/player.tsx
+++ components/player.tsx
@@ -29,6 +29,7 @@ const Player = ({ songs, activeSong }) => {
   const [repeat, setRepeat] = useState(false)
   const [shuffle, setShuffle] = useState(false)
   const [duration, setDuration] = useState(0.0)
+  const soundRef = useRef(null)
 
   const setPlayState = (value) => {
     setPlaying(value)
@@ -42,10 +43,30 @@ const Player = ({ songs, activeSong }) => {
     setRepeat((state) => !state)
   }
 
+  const prevSong = () => {
+    setIndex((state) => {
+      return state ? state - 1 : songs.length - 1
+    })
+  }
+
+  const nextSong = () => {
+    setIndex((state) => {
+      if (shuffle) {
+        const next = Math.floor(Math.random() * songs.length)
+
+        if (next === state) {
+          return nextSong()
+        }
+      } else {
+        return state === songs.length - 1 ? 0 : state + 1
+      }
+    })
+  }
+
   return (
     <Box>
       <Box>
-        <ReactHowler playing={playing} src={activeSong?.url} />
+        <ReactHowler playing={playing} src={activeSong?.url} ref={soundRef} />
       </Box>
       <Center color="gray.600">
         <ButtonGroup>
