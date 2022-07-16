01860854c393cd846fcb1823db67b5df16313274
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-07-16 11:27:31 -0700

    replace all of next.congfig.js with typescript ignore

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
-
 module.exports = {
   typescript: {
     // !! WARN !!
