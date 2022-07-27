f11721001dd0cbfc225f0f297c2607918ed8d16e
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-07-27 14:42:28 -0700

    added comments v8 - signup

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -16,38 +16,58 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   // Because emails are unique its either gonna create you because you don't exist or it's gonna say sorry this email already exist and it's gonna throw an error(That's all sign up is gonna do)
   // And then if that is successful, we'll generate a JSON Web Token and we'll save it as a cookie, so it'll be set in your browser
   // And then that cookie will be sent on every other requests that we can use to verify that is you(That is how we gonna sign you up)
+
+  // create user
   const salt = bcrypt.genSaltSync()
+  // Get the email and password(backend: making the function saying if you want to sign in, you must pass the email and password up - req.body)
   const { email, password } = req.body
 
   let user
-
+  // async await, so do some try catches here
   try {
+    // We're going to attempt to create the this user here
     user = await prisma.user.create({
       data: {
         email,
+        // for the password we want to hash it, takes in the password that they set up and the salt we created
         password: bcrypt.hashSync(password, salt),
       },
     })
+    // catch error just in case
   } catch (e) {
+    // error for email that already exist most likely
     res.status(401)
+    // send back an error property
     res.json({ error: 'User already exists' })
     return
   }
 
+  // Assuming that the user was created, we need to make a token now
+  // sign a token - this token is gonna be a long unique string
   const token = jwt.sign(
+    // hash an object that has an email, which is the email the user gave me
+    // pass the id of ther user
+    // put time of when this was created
     {
       email: user.email,
       id: user.id,
       time: Date.now(),
     },
-    // key (secret)
+    // key (secret) - this how we know that our server created this thing
     'hello',
     { expiresIn: '8h' }
   )
+  // Set this JWT in a cookie, if you set a cookie on a request, before you send it back, it's gonna get added to the browser
+  // So if we set the cookie here and we send a response back, this cookie that we're setting will get set into the person's browser that requested us
+  // local storage is fine but it can be accessed by js
+  // If we set it as a cookie and do HTTP only it cannot be accessed by js, it can only be accessed by HTTP
+  // And then you don't have to write any code on the frontend to send it up because cookies are sent up automatically anyway
+
   // Cookie is created
   res.setHeader(
     'Set-Cookie',
     // serialize wants to know the name of the cookie
+    // The name of the cookie('TRAX_ACCESS_TOKEN') and the other arg is the token that we just created
     cookie.serialize('TRAX_ACCESS_TOKEN', token, {
       // No js running in the broswer will be able to read this cookie at all
       httpOnly: true,
@@ -61,6 +81,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       secure: process.env.NODE_ENV === 'production',
     })
   )
-
+  // Send the user back
   res.json(user)
 }
